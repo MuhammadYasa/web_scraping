d9eec67e528559384b6147b74323f5b6a84eaed7
MuhammadYasa <66125818+MuhammadYasa@users.noreply.github.com>
2020-06-19 02:53:33 +0700

    menambahkan fungsi untuk melakukan running scraping

--- run.py
+++ run.py
@@ -7,7 +7,18 @@ apps = Flask(__name__)
 
 @apps.route('/')
 def home():
-    return 'Test'
+    return render_template('index.html')
+
+@apps.route('/detik-popular')
+def detik_popular():
+    html_doct = requests.get('https://www.detik.com/terpopuler',
+                             params={'tag_from': 'wp_cb_mostPopular_more'})
+    bsoup = BeautifulSoup(html_doct.text, 'html.parser')
+    popular_area = bsoup.find(attrs={'class': 'grid-row list-content'})
+    titles = popular_area.find_all(attrs={'class': 'media__title'})
+    images = popular_area.find_all(attrs={'class': 'media__image'})
+
+    return render_template('index.html',images=images)
 
 if __name__ == '__main__':
     apps.run(debug=True)
