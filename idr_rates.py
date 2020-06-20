f082a9b64a421dc0797ba207ceacc48f8dfac46c
MuhammadYasa <66125818+MuhammadYasa@users.noreply.github.com>
2020-06-20 07:20:24 +0700

    next update

--- idr_rates.py
+++ idr_rates.py
@@ -1,6 +1,6 @@
 import requests
 
-json_data = requests.get('http://www.floatrates.com/daily/idr.json')
+# json_data = requests.get('http://www.floatrates.com/daily/idr.json')
 # print(json_data.json())
 
 json_data = {"usd":{"code":"USD","alphaCode":"USD","numericCode":"840","name":"U.S. Dollar",
--- run.py
+++ run.py
@@ -7,7 +7,7 @@ apps = Flask(__name__)
 
 @apps.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('base.html')
 
 @apps.route('/detik-popular')
 def detik_popular():
@@ -17,8 +17,19 @@ def detik_popular():
     popular_area = bsoup.find(attrs={'class': 'grid-row list-content'})
     titles = popular_area.find_all(attrs={'class': 'media__title'})
     images = popular_area.find_all(attrs={'class': 'media__image'})
+    return render_template('detik-scraper.html',images=images)
 
-    return render_template('index.html',images=images)
+@apps.route('/idr-rates')
+def idr_rates():
+    source = requests.get('http://www.floatrates.com/daily/idr.json')
+    json_data = source.json()
+    return render_template('idr-rates.html', datas = json_data.values())
+
+
+# user
+@apps.route('/cards')
+def cards():
+    return render_template('/cards.html')
 
 if __name__ == '__main__':
     apps.run(debug=True)
